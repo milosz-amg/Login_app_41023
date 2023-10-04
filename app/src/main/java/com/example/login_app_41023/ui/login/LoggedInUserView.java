353acea27f5da181a7fcab2ad28b303738429386
MiloszRolewski <milosz.rolewski01@gmail.com>
2023-10-04 10:29:41 +0200

    initial commit

---
+++ app/src/main/java/com/example/login_app_41023/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package com.example.login_app_41023.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
